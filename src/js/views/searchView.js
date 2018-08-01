7a466794d678aea8e408cab4ae87238845fa032c
kevinjc <kevinjc@uio.no>
2018-08-01 20:17:42 +0200

    searchView done with title length limitation

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,5 +1,4 @@
 import { elements } from './base';
-import { access } from 'fs';
 
 export const getInput = () => elements.searchInput.value;
 
@@ -11,15 +10,15 @@ export const clearResult = () => {
     elements.searchResList.innerHTML = '';
 };
  //default length limit of the title is set to 17
-const limitRecipeTitle = (recipe, limit = 17) => {
+const limitRecipeTitle = (title, limit = 17) => {
     const newTitle = [];
     if (title.length > limit) {
-        /**
-         * 'Pasta tomato with and soup'
-         * acc:0 / accumulator + current.length = 5/ newTitle = ['Pasta']
-         * acc:5 / accumulator + current.length = 11/ newTitle = ['Pasta','tomato']
-         * acc:11 / accumulator + current.length = 15/ newTitle = ['Pasta','tomato', 'with']
-         */
+    /**
+     * string 'Pasta tomato with and soup'
+     * acc:0 / accumulator + current.length = 5/ newTitle = ['Pasta']
+     * acc:5 / accumulator + current.length = 11/ newTitle = ['Pasta','tomato']
+     * acc:11 / accumulator + current.length = 15/ newTitle = ['Pasta','tomato', 'with']
+     */
         title.split(' ').reduce((accumulator, current) => {
             if (accumulator + current.length <= limit) {
                 newTitle.push(current);
