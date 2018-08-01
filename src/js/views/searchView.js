4dc5005138845b700da3c2c02e0d063a0b665df3
kevinjc <kevinjc@uio.no>
2018-08-01 19:59:17 +0200

    limitRecipeTitle function

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,4 +1,5 @@
 import { elements } from './base';
+import { access } from 'fs';
 
 export const getInput = () => elements.searchInput.value;
 
@@ -9,6 +10,27 @@ export const clearInput = () => {
 export const clearResult = () => {
     elements.searchResList.innerHTML = '';
 };
+ //default length limit of the title is set to 17
+const limitRecipeTitle = (recipe, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        /**
+         * 'Pasta tomato with and soup'
+         * acc:0 / accumulator + current.length = 5/ newTitle = ['Pasta']
+         * acc:5 / accumulator + current.length = 11/ newTitle = ['Pasta','tomato']
+         * acc:11 / accumulator + current.length = 15/ newTitle = ['Pasta','tomato', 'with']
+         */
+        title.split(' ').reduce((accumulator, current) => {
+            if (accumulator + current.length <= limit) {
+                newTitle.push(current)
+            }
+            return accumulator + current.length
+        }, 0);
+        // return the result
+        return `${newTitle.join(' ')} ...)`
+    }
+    return title;
+}
 
 const renderRecipe = recipe => {
     const markup = `
@@ -18,7 +40,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
