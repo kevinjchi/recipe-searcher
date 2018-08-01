db08c26959bcceaff8888fc7f86b4833f78d323e
kevinjc <kevinjc@uio.no>
2018-08-02 00:56:50 +0200

    creating renderer

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /**Global state of the app
  * - Search object
@@ -21,11 +21,12 @@ const controlSearch = async () => {
     // 3) Prepare UI for results
         searchView.clearInput();
         searchView.clearResult();
-
+        renderLoader(elements.searchRes);
     // 4) Search for recipes
         await state.search.getResult();
 
     // 5) render result on UI
+        clearLoader();
         searchView.renderResult(state.search.result);
     }
 }
