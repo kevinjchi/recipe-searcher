9b6751e2e18749de2c0013b0221fc9c463dc0e70
kevinjc <kevinjc@uio.no>
2018-08-01 19:37:52 +0200

    first part of searchView

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,39 @@
 // Global app controller
-import Search from './models/Search'
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-const search = new Search('pizza');
+/**Global state of the app
+ * - Search object
+ * - current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+    // 1) getQuery from view
+    const query = searchView.getInput(); //TODO
+    if (query) {
+    // 2) new search object and add to state
+        state.search = new Search(query);
+    // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResult();
+
+    // 4) Search for recipes
+        await state.search.getResult();
+
+    // 5) render result on UI
+        searchView.renderResult(state.search.result);
+    }
+}
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+
+});
+const search = new Search('Pizza');
 console.log(search);
 search.getResult();
